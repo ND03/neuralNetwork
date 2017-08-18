c2376ea078aca3527df15568ce72398bf37b4785
eldardamirov <eldardamirov75@mail.ru>
2017-08-18 23:23:43 +0300

    Initial Commit

---
+++ neuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neuralNetwork
+//
+//  Created by Эльдар Дамиров on 18.08.17.
+//  Copyright © 2017 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
